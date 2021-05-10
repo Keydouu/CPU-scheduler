ce5bc9937d80b0780ced383fd158066506a74924
Keydouu <63008369+Keydouu@users.noreply.github.com>
2021-05-10 16:57:51 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "FilesManipulation.h"
 #include "CPUmanagement.h"
 int main()
 {
 	task* T_tasks;
 	FILE* inputFile=NULL;
-	char c;
 	int tasksNumber,i;
 	inputFile=fopen("tasksFile.txt","r");
 	if(inputFile==NULL)
 	{
         printf("file not found\n");
-        sleep(10);
         exit(0);
 	}
 	tasksNumber=numberOfTasks(inputFile);
@@ -21,11 +18,13 @@ int main()
 		*(T_tasks+i)=tasksReader(inputFile);
 	for(i=0;i<tasksNumber;i++)//on verifie que la lecture à partir du fichier a fonctionné
 		printf("%s E=%d D=%d P=%d E/S=%d\n",(T_tasks+i)->Name,(T_tasks+i)->entry,(T_tasks+i)->length,(T_tasks+i)->priority,(T_tasks+i)->inOutLength);
+	//sleep(5);
 	/*FCFS(T_tasks,tasksNumber);
 	for(i=0;i<tasksNumber;i++)
 		*(T_tasks+i)=tasksReader(inputFile);*/
-	//SJF(T_tasks,tasksNumber,0);
-	/*for(i=0;i<tasksNumber;i++)
+	SJF(T_tasks,tasksNumber,0);
+	/*rewind(inputFile);
+	for(i=0;i<tasksNumber;i++)
 		*(T_tasks+i)=tasksReader(inputFile);
 	SJF(T_tasks,tasksNumber,1);*/
 	/*for(i=0;i<tasksNumber;i++)
