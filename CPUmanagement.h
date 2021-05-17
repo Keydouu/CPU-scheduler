62ee09da0cec7e5a5dc677571034b8b3d512ade8
Keydouu <63008369+Keydouu@users.noreply.github.com>
2021-05-17 16:25:52 +0100

    Update CPUmanagement.h

--- CPUmanagement.h
+++ CPUmanagement.h
@@ -6,4 +6,5 @@ void OrgranisationSJF(int* Q,int Qsize,task* T,int newTask);
 void OrgranisationFCFS(int* Q,int newTask);
 void QueueDown(int* Q,int Qsize);
 void scheduling(task* T_tasks,int tasksNumber,int algorithm ,int preemption);
+void RR(task* T_tasks,int tasksNumber);
 #endif
