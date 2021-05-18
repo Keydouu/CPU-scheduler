18f7654133ce4d5cadf71284e60bf6066583dffb
Keydouu <63008369+Keydouu@users.noreply.github.com>
2021-05-18 13:54:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "CPUmanagement.h"
+int Menu()
+{
+	char input=0;
+	while((input<'1')||(input>'6'))
+	{
+		#if defined(_WIN32) || defined(WIN32)
+		system("cls");
+		#else
+		system("clear");
+		#endif
+		printf("1 FCFS\n");
+		printf("2 SJF sans preemption\n");
+		printf("3 SJF avec preemption\n");
+		printf("4 Par priorite sans preemption\n");
+		printf("5 Par priorite avec preemption\n");
+		printf("6 RR\n");
+		printf("entrez votre choix : \n");
+		scanf("%c",&input);
+
+	}
+	return (input-'0');
+}
 int main()
 {
 	task* T_tasks;
@@ -16,39 +38,26 @@ int main()
 	T_tasks=(task*)malloc(sizeof(task)*tasksNumber);
 	for(i=0;i<tasksNumber;i++)
 		*(T_tasks+i)=tasksReader(inputFile);
-
-	scheduling(T_tasks,tasksNumber,1,0);
-	
-	rewind(inputFile);
-	for(i=0;i<tasksNumber;i++)
-		*(T_tasks+i)=tasksReader(inputFile);
-	
-	scheduling(T_tasks,tasksNumber,2,0);
-	
-	rewind(inputFile);
-	for(i=0;i<tasksNumber;i++)
-		*(T_tasks+i)=tasksReader(inputFile);
-	
-	scheduling(T_tasks,tasksNumber,2,1);
-	
-	rewind(inputFile);
-	for(i=0;i<tasksNumber;i++)
-		*(T_tasks+i)=tasksReader(inputFile);
-	
-	scheduling(T_tasks,tasksNumber,3,0);
-	
-	rewind(inputFile);
-	for(i=0;i<tasksNumber;i++)
-		*(T_tasks+i)=tasksReader(inputFile);
-	
-	scheduling(T_tasks,tasksNumber,3,1);
-	
-	rewind(inputFile);
-	for(i=0;i<tasksNumber;i++)
-		*(T_tasks+i)=tasksReader(inputFile);
-
-    	RR(T_tasks,tasksNumber);
-	
-	fclose(inputFile);
+    fclose(inputFile);
+    switch(Menu()) {
+      case 1 :
+            scheduling(T_tasks,tasksNumber,1,0);
+         break;
+      case 2 :
+            scheduling(T_tasks,tasksNumber,2,0);
+         break;
+      case 3 :
+            scheduling(T_tasks,tasksNumber,2,1);
+         break;
+      case 4 :
+            scheduling(T_tasks,tasksNumber,3,0);
+         break;
+      case 5 :
+            scheduling(T_tasks,tasksNumber,3,1);
+         break;
+      case 6 :
+            RR(T_tasks,tasksNumber);
+         break;
+    }
 	return 0;
 }
